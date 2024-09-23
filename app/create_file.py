b5a8abfe73549be13844e66e140decb46af58340
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-23 12:39:54 +0300

    change some logic, add handler error and fix replace text in exist file

--- app/create_file.py
+++ app/create_file.py
@@ -3,21 +3,30 @@ import os
 from datetime import datetime
 
 
+class OrderError(Exception):
+    """Raise when order of command is incorrect"""
+
+
 def create_file() -> None:
-    user_input = sys.argv
-
-    for key in user_input:
-        if key == "-d":
-            key_index = user_input.index(key)
-            path_way = user_input[key_index + 1]
-
+    try:
+        user_input = sys.argv
+        
+        if "-d" in user_input and "-f" in user_input:
+            if user_input.index("-d") > user_input.index("-f"):
+                raise OrderError("Command must be like: 'python create_file.py -d {dir_name or path} -f {file_name}'")
+        
+        if "-d" in user_input:
+            key_dir_index = user_input.index("-d")
+            path_way = user_input[key_dir_index + 1]
             os.makedirs(path_way, exist_ok=True)
             os.chdir(path_way)
-        elif key == "-f":
-            key_index = user_input.index(key)
-            file_name = user_input[key_index + 1]
-
-            with open(file_name, "w") as file:
+            
+            
+        if "-f" in user_input:
+            key_file_index = user_input.index("-f")
+            file_name =  user_input[key_file_index + 1]
+            
+            with open(file_name, "a") as file:
                 file.write(datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "\n")
 
                 asking_user = True
@@ -30,6 +39,9 @@ def create_file() -> None:
                         break
 
                     file.write(new_line + "\n")
+    except ValueError as e:
+        return f"Error: {e}"
+    
 
 
 if __name__ == "__main__":
