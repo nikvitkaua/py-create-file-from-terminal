317dba69e3db75cf87d958b50d55370f8e7504b8
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-23 12:54:01 +0300

    fix flake8 app

--- app/create_file.py
+++ app/create_file.py
@@ -10,22 +10,25 @@ class OrderError(Exception):
 def create_file() -> None:
     try:
         user_input = sys.argv
-        
+
         if "-d" in user_input and "-f" in user_input:
             if user_input.index("-d") > user_input.index("-f"):
-                raise OrderError("Command must be like: 'python create_file.py -d {dir_name or path} -f {file_name}'")
-        
+                raise OrderError(
+                    "Command must be like:"
+                    "'python create_file.py"
+                    "-d {dir_name or path} -f {file_name}'"
+                )
+
         if "-d" in user_input:
             key_dir_index = user_input.index("-d")
             path_way = user_input[key_dir_index + 1]
             os.makedirs(path_way, exist_ok=True)
             os.chdir(path_way)
-            
-            
+
         if "-f" in user_input:
             key_file_index = user_input.index("-f")
-            file_name =  user_input[key_file_index + 1]
-            
+            file_name = user_input[key_file_index + 1]
+
             with open(file_name, "a") as file:
                 file.write(datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "\n")
 
@@ -41,7 +44,6 @@ def create_file() -> None:
                     file.write(new_line + "\n")
     except ValueError as e:
         return f"Error: {e}"
-    
 
 
 if __name__ == "__main__":
