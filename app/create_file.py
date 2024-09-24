80a9f2fdbfbf2cb4c7e535edba1d7ed128f3aa0e
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-24 13:29:25 +0300

    fix problems

--- app/create_file.py
+++ app/create_file.py
@@ -9,8 +9,16 @@ def create_file() -> None:
 
         if "-d" in user_input:
             key_dir_index = user_input.index("-d")
-            key_file_index = user_input.index("-f")
-            path_way = "/".join(user_input[key_dir_index + 1 : key_file_index])
+
+            path_end = 0
+
+            if "-f" in user_input:
+                path_end = user_input.index("-f")
+            else:
+                path_end = len(user_input)
+
+            path_way = "/".join(user_input[key_dir_index + 1 : path_end])
+
             os.makedirs(path_way, exist_ok=True)
             os.chdir(path_way)
 
@@ -18,8 +26,11 @@ def create_file() -> None:
             key_file_index = user_input.index("-f")
             file_name = user_input[key_file_index + 1]
 
-            with open(file_name, "a") as file:
-                file.write(datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "\n")
+            with open(file_name, "a+") as file:
+                if file.tell() > 0:
+                    file.write("\n")
+
+                file.write(datetime.now().strftime("%m-%d-%Y %H:%M:%S" + "\n"))
 
                 asking_user = True
 
@@ -27,13 +38,12 @@ def create_file() -> None:
                     new_line = input("Enter content line: ")
 
                     if new_line == "stop":
-                        file.write("\n")
                         asking_user = False
                         break
 
                     file.write(new_line + "\n")
-    except ValueError as e:
-        return f"Error: {e}"
+    except ValueError:
+        return "Incorrect value"
 
 
 if __name__ == "__main__":
